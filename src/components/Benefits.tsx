6c7a807c06eecf488d2820674bde56dacc62df4e
Abhiroop CVK <hermitdudz@gmail.com>
2024-09-19 15:38:38 -0700

    fixed the rest !

---
+++ src/components/Benefits.tsx
@@ -0,0 +1,22 @@
+const Benefits = ({ benefits, title }: { benefits: Array<{ title: string; description: string }>; title: string }) => {
+    return (
+        <section aria-labelledby="features" id="feature-five" className="w-full py-24 px-4 sm:px-6 lg:px-8 bg-white">
+            <div className='max-w-6xl mx-auto'>
+                <h1 className='text-2xl font-mono pb-12'>{title}</h1>
+
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-12">
+                    {benefits.map((benefit, index) => (
+                        <div key={index}>
+                            <p className="mt-3 text-lg font-medium leading-6">{benefit.title}</p>
+                            <div className="mt-1 text-base text-gray-500">
+                                {benefit.description}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Benefits;
