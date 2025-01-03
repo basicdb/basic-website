46f7012eb16832ea69562d0e9bc1083df41a8ad9
Abhiroop CVK <hermitdudz@gmail.com>
2025-01-03 13:47:25 -0800

    implemented cal.com

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,11 +12,18 @@ import { JetBrains_Mono, Lora } from 'next/font/google'; // Update font imports
 import CodeBlock from '@/components/CodeBlock';
 import products from '@/utils/productsData';
 import developerFeatures from '@/utils/developerFeaturesData';
+import Cal, { getCalApi } from "@calcom/embed-react";
 
 const jetbrainsMono = JetBrains_Mono({ subsets: ['latin'] }); // Initialize JetBrains Mono
 const lora = Lora({ subsets: ['latin'] }); // Initialize Lora
 
 export default function Home() {
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi({ "namespace": "speak-to-a-basic-founder" });
+      cal("ui", { "hideEventTypeDetails": false, "layout": "month_view" });
+    })();
+  }, [])
   return (
     <>
       <Head>
@@ -35,6 +42,11 @@ export default function Home() {
           <PrivacySection />
           <BenefitsForUsers benefits={benefitsForUsers} />
           <PricingSection />
+          <Cal namespace="speak-to-a-basic-founder"
+            calLink="abhicvk/speak-to-a-basic-founder"
+            style={{ width: "100%", height: "100%", overflow: "scroll", backgroundColor: "#FFF3EB", padding: "20px" }}
+            config={{ "layout": "month_view" }}
+          />
           <Footer />
         </div>
       </main>
