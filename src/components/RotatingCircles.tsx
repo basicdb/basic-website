f659ba958caaa02d1637137adbe0f0a02d73f4ba
Abhiroop CVK <hermitdudz@gmail.com>
2025-01-20 19:15:40 -0800

    merged into just one circle

--- src/components/RotatingCircles.tsx
+++ src/components/RotatingCircles.tsx
@@ -16,16 +16,14 @@ export function OrbitingCirclesDemo() {
     return (
         <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden">
             <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-black">
-                <Image src="/user-owned-cylinder.svg" alt="user-owned" width={325} height={325} />
+                <Image src="/user-owned-cylinder.svg" alt="user-owned" width={300} height={300} />
             </span>
 
-            <OrbitingCircles iconSize={20} radius={180} path={false} speed={0.7} className="text-green-700 dark:text-green-400">
+            <OrbitingCircles iconSize={20} radius={180} path={false} speed={0.2} className="text-green-700 dark:text-green-400">
                 <BadgeDollarSign />
                 <NotebookTextIcon />
                 <MessagesSquareIcon />
                 <MailIcon />
-            </OrbitingCircles>
-            <OrbitingCircles radius={95} path={false} speed={1.2} className="text-green-500">
                 <FileImageIcon />
                 <FingerprintIcon />
                 <IdCardIcon />
