8a62a8aa27998b5334c3eae05f143ce0eac2eb7b
Abhiroop CVK <hermitdudz@gmail.com>
2025-03-07 11:16:34 -0800

    updated lofihackrecap video to be on s3

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ const heroCardElements = {
     buttonBorderColor: 'border-indigo-700/80 dark:border-indigo-700/70',
     buttonIcon: <StarIcon className='w-5 h-5' />,
     buttonColor: 'bg-gradient-to-r from-green-700/80 to-indigo-700/80 dark:from-green-700/70 dark:to-indigo-700/70',
-    video: '/lofihack-recap.mp4'
+    video: 'https://basicwebsitecontent.s3.us-east-2.amazonaws.com/Event+Recap_v3.mp4'
 }
 
 export default function Hero() {
