1412d929c1743ad963a3946adf60eae29be942e8
Abhiroop CVK <hermitdudz@gmail.com>
2025-07-15 19:34:01 -0700

    update fonts to overpass

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
 	],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ['Overpass', 'sans-serif'],
+				heading: ['Overpass', 'sans-serif'],
+			},
 			colors: {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
