4c79cbe2081a216ba431a00217c94d99f6c65505
Rashid Aziz <aziz@sophire.com>
2025-11-05 11:46:14 -0800

    build err fix

---
+++ src/types/react-icons.d.ts
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+declare module 'react-icons/ri' {
+  export const RiTwitterXLine: React.FC<React.SVGProps<SVGSVGElement>>;
+  export const RiBlueskyLine: React.FC<React.SVGProps<SVGSVGElement>>;
+}
+
+declare module 'react-icons/fa' {
+  export const FaDiscord: React.FC<React.SVGProps<SVGSVGElement>>;
+}
+
